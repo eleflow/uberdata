7a1cbd025de43437b0a4e464eff8c5d4c52257c5
Caio Martins <caioagmartins@gmail.com>
2016-08-08 11:01:50 -0300

    Updating travis resource

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 git clone https://github.com/sryza/spark-timeseries.git
 cd spark-timeseries
-mvn package install -DskipTests -Dgpg.skip
+sudo mvn package install -DskipTests -Dgpg.skip
