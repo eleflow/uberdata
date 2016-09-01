eee6cc61c8032ac7a97dde4e2d0f1f06732f0a2a
Caio Martins <caioagmartins@gmail.com>
2016-09-01 11:48:53 -0300

    Add circleci file and xgboost dependency

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+echo "clonning and building sparkts"
 git clone https://github.com/sryza/spark-timeseries.git
 cd spark-timeseries
 mvn package install --quiet -DskipTests -Dgpg.skip
+
+cd ../
+echo "clonning and building xgboost"
+git clone https://github.com/dmlc/xgboost
+cd xgboost/jvm-packages/
+mvn package install -DskipTests
